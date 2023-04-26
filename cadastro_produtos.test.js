6d2aec83645e967621874bca1ff338cbb14279a5
LUIS HENRIQUE LEAL RIES <LHRIES@fspoa.br>
2023-04-25 21:55:47 -0300

    Iniciando os testes unitarios

---
+++ cadastro_produtos.test.js
@@ -0,0 +1,6 @@
+const cadastro = require('./cadastro_produtos')
+
+test('Listar Produtos sem cadastrar deve retornar vazio', () => {
+    expect(cadastro.listar()).toEqual([]);
+    expect(cadastro.listar().length).toBe(0);
+})
